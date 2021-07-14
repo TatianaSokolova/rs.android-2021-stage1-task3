dbc0c346bc21b4b259f47abaa30c7707f21091d4
TatianaSokolova <Stanya0427@gmail.com>
2021-07-14 23:38:11 +0300

    final update task2

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,54 +1,32 @@
 package subtask2
 
-import kotlin.math.pow
-import kotlin.math.sqrt
 
 class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        if (number < 5) {
-            return null
-        }
-        fun square(n: Int): Long = n.toLong() * n.toLong()
+//        if (number < 5) {
+//            return null
+//        }
 
+        fun square(n: Int): Long = n.toLong() * n
         val numberToDecompose = square(number)
 
-        val result = mutableListOf<Int>()
+        fun findNumbers(n: Long, k: Int):  Array<Int>? {
+            for (i in k - 1 downTo 1) {
+                if (square(i) == n) return Array(1) {i}
 
-        var f: Long = 0
-
-//        for (i in numberToDecompose downTo 0) {
-//            if (i.toDouble().pow(2).toInt() < numberToDecompose) {
-//                firstNum = i.toInt()
-//                result.add(i.toInt())
-//                break
-//            }
-//        }
-//
-//        var rest = number - firstNum
-
-//        fun check(list: MutableList<Int>): Boolean {
-//            val checkResult = mutableSetOf<Int>()
-//            checkResult.addAll(list)
-//            return result.size == checkResult.size
-//        }
-
-        fun findNumbers(n: Long, k: Int): MutableList<Int> {
-            for (i in k-1 downTo 1) {
-                if (square(k) <= n) {
-                    result.add(i)
-                    val rest = n - square(i)
-                    val next = sqrt(rest.toDouble()).toInt()
-                    if (rest.toInt() == 1) {
-                        result.add(1)
+                val rest = n - square(i)
+                if (rest > 0L) {
+                    val list = findNumbers(rest, i)
+                    if (list != null) {
+                        return list.plus(i)
                     }
                 }
             }
-            return result
+            return null
         }
 
-
-        return findNumbers(numberToDecompose, number).toTypedArray()
+        return findNumbers(numberToDecompose, number)
     }
 }
